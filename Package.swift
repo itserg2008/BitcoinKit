560fbf5ec7db3b4c2192e52ffb4a73cc0ef5c213
Sergey D <sergeypk2008@gmail.com>
2024-04-17 15:18:27 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .library(name: "BitcoinKit", targets: ["BitcoinKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor-community/copenssl.git", .exact("1.0.0-rc.1")),
+        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", .upToNextMinor(from: "3.1.5000")),
         .package(url: "https://github.com/Boilertalk/secp256k1.swift", .upToNextMinor(from: "0.1.0")),
         .package(url: "https://github.com/vapor-community/random.git", .upToNextMinor(from: "1.2.0"))
     ],
@@ -18,7 +18,7 @@ let package = Package(
         ),
         .target(
             name: "BitcoinKitPrivate",
-            dependencies: ["COpenSSL", "secp256k1"]
+            dependencies: ["OpenSSL", "secp256k1"]
         ),
         .testTarget(
             name: "BitcoinKitTests",
