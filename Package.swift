66d2995ac78b240783d7fbf045b38b218d7eb094
Sergey D <sergeypk2008@gmail.com>
2024-04-17 16:19:48 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "BitcoinKit",
     platforms: [
         .iOS(.v12),
-        .macOS(.v10_15)        
+        .macOS(.v10_15)
     ],
     products: [
         .library(name: "BitcoinKit", targets: ["BitcoinKit"])
